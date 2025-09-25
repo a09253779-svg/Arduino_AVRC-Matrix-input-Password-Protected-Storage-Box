1c40b7762dfe0013f25d47d6ba742c105b0f18ba
Timmy <a09253779@gmail.com>
2025-09-25 23:43:53 +0800

    新增matrix input password box project

---
+++ README.md
@@ -0,0 +1,23 @@
+
+1.AVRC 語法，採用4X3矩陣輸入和而外1個重製密碼輸入鍵組成的密碼置物箱
+
+2.shiftOut_LSBFIRST():
+    //8個bit一個一個丟進74HC595N
+    // 若當下的bit=1 設定DATA_PIN=1 否則 設定DATA_PIN=0
+    //CLOCK_PIN從1到0時賣變化代表將前一個bit推入74HC595N
+    //LATCH_PIN=0 代表先不輸出 LATCH_PIN=1 將8個bit同步輸出
+
+3.servo_init():
+    使用 Timer1, Fast PWM, 20ms 週期
+    頻率=F_CPU / (預分頻 * TOP+1)
+    detail:
+    // Fast PWM, Prescale=8 (預分頻8)
+    //F_CPU=16MHz, 預分頻=8 => 2MHz =>0.5msec (1tick/0.5msec)
+    // TOP: 20ms 設定週期長度ICR1+1=40000 tick => 40000*0.5=200sec (50Hz)
+    // PB1 = OC1A 設為輸出 (COM1A1=1)
+    
+4.servo_write():
+    // 0°=1ms,180°=2ms
+    // 1ms=1000us,2ms=2000us
+    //pulse 單位us
+    //OCR1A 單位tick (輸入第幾個tick變低電位，總長度40000 tick)
---
+++ box.c
@@ -0,0 +1,180 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define maxsizepassword 4
+ 
+#define matrixout0 PD5   
+#define matrixout1 PD6
+#define matrixout2 PD7
+#define matrixin0 PD1
+#define matrixin1 PD2
+#define matrixin2 PD3
+#define matrixin3 PD4
+
+#define resetpin  PB0
+#define servopin  PB1
+#define CLOCK_PIN  PB2  
+#define LATCH_PIN  PB3  
+#define DATA_PIN   PB4  
+
+void write_password(int* input_password,int* indx,int val){
+    if(* indx==maxsizepassword)
+        return;
+    input_password[(*indx)++]=val;
+}
+void reset_input(int* indx){
+    *indx=0;
+}
+
+int is_password_true(int* password,int* input_password){
+    for(int i=0;i<maxsizepassword;i++){
+        if(password[i]!=input_password[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void set_password(int* password,int* setindx, int val){
+    if(* setindx==maxsizepassword)
+        return;
+    password[(* setindx)++]=val;
+}
+
+
+void shiftOut_LSBFIRST(uint8_t val){
+    for (uint8_t i=0;i<8;i++){
+
+        if(val&0x01){
+            PORTB|=(1<<DATA_PIN);   
+        } else {
+            PORTB&=~(1<<DATA_PIN);  
+        }
+
+        PORTB|=(1<<CLOCK_PIN);
+        PORTB&=~(1<<CLOCK_PIN);
+        val>>=1;                     
+    }
+}
+
+uint8_t segCode[11] = {0xFC,0x60,0xDA,0xF2,0x66,0xB6,0xBE,0xE0,0xFE,0xF6,0x00};
+void display(int num){
+    if(num>=0&&num<11){
+        PORTB&=~(1<<LATCH_PIN);                   
+        shiftOut_LSBFIRST(segCode[num]);         
+        PORTB|=(1<<LATCH_PIN);                    
+    }
+}
+
+
+void servo_init() {
+    ICR1=39999;                              
+    TCCR1A =(1<<COM1A1)|(1<<WGM11); 
+    TCCR1B =(1<<WGM13)|(1<<WGM12)|(1<<CS11); 
+    DDRB |=(1<<servopin);                    
+}
+
+// 設定角度 0~180
+void servo_write(uint8_t angle) {
+    uint16_t pulse=1000+((angle*1000UL)/180); 
+    OCR1A=pulse*2;                         
+}
+
+
+int main(){
+    int password[4]={0,0,0,0};
+    int input_password[4];
+    int indx=0,setindx=0;
+    int setmode=0;
+    int keymap[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,0,11}};
+
+    
+    DDRD|=(1<<matrixout0);
+    DDRD|=(1<<matrixout1);
+    DDRD|=(1<<matrixout2);
+
+
+    DDRD &= ~(1<<matrixin0);
+    DDRD &= ~(1<<matrixin1);
+    DDRD &= ~(1<<matrixin2);
+    DDRD &= ~(1<<matrixin3);
+
+    PORTD|=(1<<matrixout0)|(1<<matrixout1)|(1<<matrixout2);
+    PORTD|=(1<<matrixin0)|(1<<matrixin1)|(1<<matrixin2)|(1<<matrixin3);
+    
+
+
+    DDRB &= ~(1<<resetpin);
+    PORTB|=(1<<resetpin);
+
+    DDRB|=(1<<DATA_PIN)|(1<<CLOCK_PIN)|(1<<LATCH_PIN); 
+    servo_init();
+
+    while(1){
+
+        if(!(PINB&(1<<resetpin))){
+            setmode=1;
+            reset_input(&setindx);
+            _delay_ms(50);
+        }
+
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                int matrixoutpin=matrixout0+j;
+                int matrixinpin=matrixin0+i;
+                PORTD&=~(1<<matrixoutpin);
+
+                if(!(PIND&(1<<matrixinpin))){
+                    int val=keymap[i][j];
+                    if(setmode){
+                        if(val==10){
+                            if(setindx==4){
+                                setmode=0;
+                                servo_write(180);
+                                display(10);
+                            }
+                            reset_input(&setindx);
+                        }
+                        else if(val==11){
+                            reset_input(&setindx);
+                        }
+                        else{
+                            if(setindx<maxsizepassword){
+                                set_password(password,&setindx,val);
+                                display(val);
+                            }
+
+                        }
+                    }
+                    else{
+                        if(val==10){
+                            if(indx==4){
+                                if(is_password_true(password,input_password)){
+                                    servo_write(0);
+                                }
+                            }
+                            reset_input(&indx);
+                        }
+                        else if(val==11){
+                            reset_input(&indx);
+                        }
+                        else{
+                            if(indx<maxsizepassword){
+                                write_password(input_password,&indx,val);
+                                display(val);
+                            }
+
+                        }    
+                    }
+                    _delay_ms(200);
+                }
+                PORTD|=(1<<matrixoutpin);
+            }
+        }
+
+    }
+
+    return 0;
+}
